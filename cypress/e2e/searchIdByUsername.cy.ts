0a8802d7e0f74db871b15583725f60142cd30255
AutoShan <shanrielalarcon@gmail.com>
2022-08-16 12:15:47 -0400

    update

--- cypress/e2e/searchIdByUsername.cy.ts
+++ cypress/e2e/searchIdByUsername.cy.ts
@@ -1,26 +1,34 @@
 import dataSource from '../fixtures/dataSource.json'
 
 describe('searchIdByUsername.cy', () => {
-    it('Search User', () => {
-      let res;
-      cy.request ({
-        method: 'GET',
-        url: '/users?username=' + dataSource.username
-      }).then(res => {
+  it('Search User', () => {
+    cy.request ({
+      method: 'GET',
+      url: '/users?username=' + dataSource.username
+    })
+      .then(res => {
         expect(res.status).to.eq(200);
         expect(res.body[0].username).equal('Delphine');
         cy.log(res.body[0].id);
       });
-      });
+  });
 
-    it.only('Find Specific User', () => {
-      let res;
-      cy.request ({
-        method: 'GET',
-        url: '/users'
-      }).then(res => {
-        expect(res.status).to.eq(200);
-        expect(res.body).to.not.be.null;
-      })
+  it.only('Find Specific User', () => {
+    cy.request ({
+      method: 'GET',
+      url: '/users'
     })
+      .then(res => {
+        expect(res.status).to.eq(200);
+        expect(res.body).to.not.null;
+        
+        var arrUsers = res.body;
+
+        for (var index in arrUsers) {
+          cy.log(arrUsers[index].username + ' ' + arrUsers[index].id)
+        }
+
+      });
+  })
+
 });
