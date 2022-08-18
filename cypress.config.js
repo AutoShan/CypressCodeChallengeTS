0058ede230777643f025c8a0e825738bbc206429
AutoShan <shanrielalarcon@gmail.com>
2022-08-18 06:58:38 -0400

    Added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://jsonplaceholder.typicode.com',
     setupNodeEvents(on, config) {
+
     },
+  },
+  video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 });
