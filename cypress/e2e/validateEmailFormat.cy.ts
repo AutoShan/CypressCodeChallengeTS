67035c78aaa5bf1f3dc25b4a72bcf7c2e180d7ca
AutoShan <shanrielalarcon@gmail.com>
2022-08-18 03:27:57 -0400

    Added comments modified:   cypress/e2e/validateEmailFormat.cy.ts

--- cypress/e2e/validateEmailFormat.cy.ts
+++ cypress/e2e/validateEmailFormat.cy.ts
@@ -1,14 +1,19 @@
 import dataSource from "../fixtures/dataSource.json";
 
 describe("validateEmailFormat.cy", () => {
+
   it("Validate Email Address Format", () => {
+
+    //Call getuserIdByUsername command to get the userId of specific user (Delphine)
     cy.getuserIdByUsername(dataSource.username).then((returnedUserId) => {
       cy.log(dataSource.username + " userId is " + returnedUserId);
 
+      //Call getPostByUserId command to get the posts made by the specific userId
       cy.getPostByUserId(dataSource.urlPostByUser, returnedUserId).then(
         (returnedPostByUserId) => {
           let posts = returnedPostByUserId;
 
+          //Iterate on each post
           posts.forEach((element) => {
             cy.request({
               method: "GET",
@@ -18,11 +23,12 @@ describe("validateEmailFormat.cy", () => {
               expect(res.body).to.not.be.null;
               expect(res.body[0].postId).to.eq(element.id);
               let arrComments = res.body;
-
+              
+              //Iterate in the posted comments to validate each emails
               for (var index in arrComments) {
                 cy.validateEmailFormat(arrComments[index].email).then(
                   (bResult) => {
-                    cy.log(arrComments[index].email);
+                    cy.log('Email address is valid: ' + arrComments[index].email);
                     expect(bResult).to.eq(true);
                   }
                 );
